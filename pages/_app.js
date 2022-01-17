a97d9ef90ea6b5ae65f40fcb57ab05e122a2d7c5
Alejandro Camarillo <tribak.zero@gmail.com>
2022-01-16 22:00:32 -0600

    Feat: PoC

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+
 import '../styles/globals.css'
 
+const queryClient = new QueryClient()
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+    )
 }
 
 export default MyApp
